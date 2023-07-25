741ea6f66557caa96906deb6cdef5e7c0148df5a
Helen Lu <109957051+hlu750@users.noreply.github.com>
2023-07-25 19:56:32 +1200

    Add files via upload

---
+++ submission/script.js
@@ -0,0 +1,11 @@
+/*
+    Carbon Easy! (CE) web application script.
+    Developed by Helen Lu.
+*/
+
+/**
+ * This function is used to locate the modal element, and present it on the viewport.
+ */
+function register() { 
+  document.getElementById("register").style.display = "block";
+}
